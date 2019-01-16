79c29e21b77d2220f1e50ff4922cdfa763e3cbd6
Bradyn Golda <bgolda89@gmail.com>
2019-01-16 15:00:28 -0700

    Added environment to girignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB474BhSmZP8_5OR1s1JzUWSiAQP5_LkDg",
+    authDomain: "century-1b788.firebaseapp.com",
+    databaseURL: "https://century-1b788.firebaseio.com",
+    projectId: "century-1b788",
+    storageBucket: "century-1b788.appspot.com",
+    messagingSenderId: "549495999845"
+  }
 };
 
 /*
